c94bae54c44a309b1cceef0a0b9e183243a69ef7
Evgene Petrenko <mrEvgenix@gmail.com>
2021-05-26 08:19:25 +0700

    Fix middleware

--- auxy/bot/middleware.py
+++ auxy/bot/middleware.py
@@ -1,29 +1,24 @@
-import asyncio
+import logging
 from aiogram import types
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.dispatcher.handler import CancelHandler
-from auxy.settings import SUPERUSERS
 
 
 class WhitelistMiddleware(BaseMiddleware):
     
     def __init__(self, users_whitelist=None):
         super().__init__()
-        self.users_whitelist = users_whitelist if not users_whitelist else []
+        self.users_whitelist = users_whitelist if users_whitelist else []
 
     async def on_pre_process_message(self, message: types.Message, data: dict):
         sender = message['from']
-        bot = self.manager.bot
         if self.users_whitelist and sender['id'] not in self.users_whitelist:
             await message.answer(
                 'Извините, но пока мой создатель не дает добро на общение с вами.\n'
                 'Если что-то поменяется, я постараюсь вам это сообщить.'
             )
-            for superuser_id in SUPERUSERS:
-                await bot.send_message(
-                    superuser_id,
-                    f'Пользователь {sender["first_name"]} под ником @{sender["username"]} с id={sender["id"]}'
-                    f'отправил мне следующее сообщение:\n{message["text"]}'
-                )
-                await asyncio.sleep(.05)
+            logging.info(
+                f'User {sender["first_name"]} (@{sender["username"]}) id={sender["id"]} '
+                f'message: {message["text"]}'
+            )
             raise CancelHandler()
--- auxy/settings.py
+++ auxy/settings.py
@@ -1,7 +1,5 @@
 import os
 
-SUPERUSERS = list(map(int, os.environ['AUXY_SUPERUSERS'].split(',')))
-
 WHITELISTED_USERS = list(map(int, os.environ['AUXY_WHITELISTED_USERS'].split(',')))
 
 TELEGRAM_BOT_API_TOKEN = os.environ['AUXY_TELEGRAM_BOT_API_TOKEN']
